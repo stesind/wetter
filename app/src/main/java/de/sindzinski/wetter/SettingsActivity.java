886cc9f870e5b9fe963526090ae920db08c85a68
Steffen Sindzinski <steffen.sindzinski@gmail.com>
2017-01-15 15:28:06 +0100

    changed preference activity to preference fragment

--- app/src/main/java/de/sindzinski/wetter/SettingsActivity.java
+++ app/src/main/java/de/sindzinski/wetter/SettingsActivity.java
@@ -16,6 +16,7 @@
 package de.sindzinski.wetter;
 
 import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
@@ -23,7 +24,9 @@ import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.widget.Toast;
 
 import de.sindzinski.wetter.data.WeatherContract;
 import de.sindzinski.wetter.sync.WetterSyncAdapter;
@@ -36,116 +39,136 @@ import de.sindzinski.wetter.sync.WetterSyncAdapter;
  * href="http://developer.android.com/guide/topics/ui/settings.html">Settings
  * API Guide</a> for more information on developing a Settings UI.
  */
-public class SettingsActivity extends PreferenceActivity
-        implements Preference.OnPreferenceChangeListener, SharedPreferences.OnSharedPreferenceChangeListener {
+public class SettingsActivity extends Activity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Add 'general' preferences, defined in the XML file
-        addPreferencesFromResource(R.xml.pref_general);
 
-        // For all preferences, attach an OnPreferenceChangeListener so the UI summary can be
-        // updated when the preference changes.
-        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
-        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_units_key)));
-        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_art_pack_key)));
+
+        // Display the fragment as the main content.
+        getFragmentManager().beginTransaction().replace(android.R.id.content,
+                new PrefsFragment()).commit();
     }
 
-    // Registers a shared preference change listener that gets notified when preferences change
-    @Override
-    protected void onResume() {
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
-        sp.registerOnSharedPreferenceChangeListener(this);
-        super.onResume();
-    }
+    public static class PrefsFragment extends PreferenceFragment
+            implements
+            Preference.OnPreferenceChangeListener,
+            SharedPreferences.OnSharedPreferenceChangeListener {
 
-    // Unregisters a shared preference change listener
-    @Override
-    protected void onPause() {
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
-        sp.unregisterOnSharedPreferenceChangeListener(this);
-        super.onPause();
-    }
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            // Add 'general' preferences, defined in the XML file
+            addPreferencesFromResource(R.xml.pref_general);
 
-    /**
-     * Attaches a listener so the summary is always updated with the preference value.
-     * Also fires the listener once, to initialize the summary (so it shows up before the value
-     * is changed.)
-     */
-    private void bindPreferenceSummaryToValue(Preference preference) {
-        // Set the listener to watch for value changes.
-        preference.setOnPreferenceChangeListener(this);
+            // For all preferences, attach an OnPreferenceChangeListener so the UI summary can be
+            // updated when the preference changes.
+            bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
+            bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_units_key)));
+            bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_art_pack_key)));
+            bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_theme_key)));
 
-        // Set the preference summaries
-        setPreferenceSummary(preference,
-                PreferenceManager
-                        .getDefaultSharedPreferences(preference.getContext())
-                        .getString(preference.getKey(), ""));
-    }
-
-    private void setPreferenceSummary(Preference preference, Object value) {
-        String stringValue = value.toString();
-        String key = preference.getKey();
-
-        if (preference instanceof ListPreference) {
-            // For list preferences, look up the correct display value in
-            // the preference's 'entries' list (since they have separate labels/values).
-            ListPreference listPreference = (ListPreference) preference;
-            int prefIndex = listPreference.findIndexOfValue(stringValue);
-            if (prefIndex >= 0) {
-                preference.setSummary(listPreference.getEntries()[prefIndex]);
-            }
-        } else if (key.equals(getString(R.string.pref_location_key))) {
-            @WetterSyncAdapter.LocationStatus int status = Utility.getLocationStatus(this);
-            switch (status) {
-                case WetterSyncAdapter.LOCATION_STATUS_OK:
-                    preference.setSummary(stringValue);
-                    break;
-                case WetterSyncAdapter.LOCATION_STATUS_UNKNOWN:
-                    preference.setSummary(getString(R.string.pref_location_unknown_description, value.toString()));
-                    break;
-                case WetterSyncAdapter.LOCATION_STATUS_INVALID:
-                    preference.setSummary(getString(R.string.pref_location_error_description, value.toString()));
-                    break;
-                default:
-                    // Note --- if the server is down we still assume the value
-                    // is valid
-                    preference.setSummary(stringValue);
-            }
-        } else {
-            // For other preferences, set the summary to the value's simple string representation.
-            preference.setSummary(stringValue);
+            // set texts correctly
+            onSharedPreferenceChanged(null, "");
         }
 
-    }
+        // Registers a shared preference change listener that gets notified when preferences change
+        @Override
+        public void onResume() {
+            super.onResume();
+            getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+        }
 
-    // This gets called before the preference is changed
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object value) {
-        setPreferenceSummary(preference, value);
-        return true;
-    }
+        // Unregisters a shared preference change listener
+        @Override
+        public void onPause() {
+            super.onPause();
+            getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+        }
 
-    // This gets called after the preference is changed, which is important because we
-    // start our synchronization here
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        if ( key.equals(getString(R.string.pref_location_key)) ) {
-            // we've changed the location
-            // first clear locationStatus
-            Utility.resetLocationStatus(this);
-            WetterSyncAdapter.syncImmediately(this);
-        } else if ( key.equals(getString(R.string.pref_units_key)) ) {
-            // units have changed. update lists of weather entries accordingly
-            getContentResolver().notifyChange(WeatherContract.WeatherEntry.CONTENT_URI, null);
-        } else if ( key.equals(getString(R.string.pref_location_status_key)) ) {
-            // our location status has changed.  Update the summary accordingly
-            Preference locationPreference = findPreference(getString(R.string.pref_location_key));
-            bindPreferenceSummaryToValue(locationPreference);
-        } else if ( key.equals(getString(R.string.pref_art_pack_key)) ) {
-            // art pack have changed. update lists of weather entries accordingly
-            getContentResolver().notifyChange(WeatherContract.WeatherEntry.CONTENT_URI, null);
+        /**
+         * Attaches a listener so the summary is always updated with the preference value.
+         * Also fires the listener once, to initialize the summary (so it shows up before the value
+         * is changed.)
+         */
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            // Set the listener to watch for value changes.
+            preference.setOnPreferenceChangeListener(this);
+
+            // Set the preference summaries
+            setPreferenceSummary(preference,
+                    PreferenceManager
+                            .getDefaultSharedPreferences(preference.getContext())
+                            .getString(preference.getKey(), ""));
+        }
+
+        private void setPreferenceSummary(Preference preference, Object value) {
+            String stringValue = value.toString();
+            String key = preference.getKey();
+
+            if (preference instanceof ListPreference) {
+                // For list preferences, look up the correct display value in
+                // the preference's 'entries' list (since they have separate labels/values).
+                ListPreference listPreference = (ListPreference) preference;
+                int prefIndex = listPreference.findIndexOfValue(stringValue);
+                if (prefIndex >= 0) {
+                    preference.setSummary(listPreference.getEntries()[prefIndex]);
+                }
+            } else if (key.equals(getString(R.string.pref_location_key))) {
+                @WetterSyncAdapter.LocationStatus int status = Utility.getLocationStatus(getActivity());
+                switch (status) {
+                    case WetterSyncAdapter.LOCATION_STATUS_OK:
+                        preference.setSummary(stringValue);
+                        break;
+                    case WetterSyncAdapter.LOCATION_STATUS_UNKNOWN:
+                        preference.setSummary(getString(R.string.pref_location_unknown_description, value.toString()));
+                        break;
+                    case WetterSyncAdapter.LOCATION_STATUS_INVALID:
+                        preference.setSummary(getString(R.string.pref_location_error_description, value.toString()));
+                        break;
+                    default:
+                        // Note --- if the server is down we still assume the value
+                        // is valid
+                        preference.setSummary(stringValue);
+                }
+            } else {
+                // For other preferences, set the summary to the value's simple string representation.
+                preference.setSummary(stringValue);
+            }
+        }
+
+        // This gets called before the preference is changed
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            setPreferenceSummary(preference, value);
+            return true;
+        }
+
+        // This gets called after the preference is changed, which is important because we
+        // start our synchronization here
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            if (key.equals(getString(R.string.pref_location_key))) {
+                // we've changed the location
+                // first clear locationStatus
+                Utility.resetLocationStatus(getActivity());
+                WetterSyncAdapter.syncImmediately(getActivity());
+            } else if (key.equals(getString(R.string.pref_units_key))) {
+                // units have changed. update lists of weather entries accordingly
+                getActivity().getContentResolver().notifyChange(WeatherContract.WeatherEntry.CONTENT_URI, null);
+            } else if (key.equals(getString(R.string.pref_location_status_key))) {
+                // our location status has changed.  Update the summary accordingly
+                Preference locationPreference = findPreference(getString(R.string.pref_location_key));
+                bindPreferenceSummaryToValue(locationPreference);
+            } else if (key.equals(getString(R.string.pref_art_pack_key))) {
+                // art pack have changed. update lists of weather entries accordingly
+                getActivity().getContentResolver().notifyChange(WeatherContract.WeatherEntry.CONTENT_URI, null);
+            } else if (key.equals(getString((R.string.pref_theme_key)))) {
+                // toast restart app required
+                Toast.makeText(getActivity(), getString(R.string.notification_restart), Toast.LENGTH_SHORT).show();
+
+            }
         }
     }
 
